f8137bc9d40219452da573a4eda1e128f35c33de
developmentrankmantra-sudo <developmentrankmantra@gmail.com>
2025-12-27 13:06:29 +0530

    cnext js confilt sovle

--- next.config.js
+++ next.config.js
@@ -1,10 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  output: "export",
+  trailingSlash: true,     // 🔥 VERY IMPORTANT
   eslint: {
     ignoreDuringBuilds: true,
   },
-  images: { unoptimized: true },
+  images: {
+    unoptimized: true,
+  },
 };
 
 module.exports = nextConfig;
